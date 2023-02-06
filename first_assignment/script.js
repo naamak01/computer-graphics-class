6cab671af3a0b80f93e007da51ecaeebf6d0623b
Nana Amakye <noamakye@gmail.com>
2023-02-06 08:40:23 -0600

    Added new scripts and changed some names around the code

--- first_assignment/script.js
+++ first_assignment/script.js
@@ -1,199 +1,84 @@
-var scene, camera, renderer, envMap, phongMaterial, standardMaterial, params1, params2, faceNormalsHelper, vertexNormalsHelper;
+var scene, camera, renderer, skyboxGeo, skybox;
 
 init();
 
 function init(){
-  const assetPath = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/';
+
+    const assetPath = '..Computer_Graphics/three.js/pictures';
   
-  envMap = new THREE.CubeTextureLoader().setPath(`${assetPath}skybox3_`).load([
-    'px.jpg', 'nx.jpg', 
-    'py.jpg', 'ny.jpg', 
-    'pz.jpg', 'nz.jpg'
-  ])
+    envMap = new THREE.CubeTextureLoader().setPath(`${assetPath}skybox3_`).load([
+      'ft.jpg', 'bk.jpg', 
+      'up.jpg', 'dn.jpg', 
+      'rt.jpg', 'lf.jpg'
+    ])
+
+    skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+    skybox = new THREE.Mesh(skyboxGeo);
+    scene.add(skybox);
+  
+    animate();
+
+
   scene = new THREE.Scene();
-  scene.background = envMap;
-  
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set(0, 0, 10);
-  
+  camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 45, 3000 );
+  camera.position.set(1200, -250, 20000);
+
   const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
   scene.add(ambient);
-  
-  const light = new THREE.DirectionalLight(0xFFFFFF, 3);
-  light.position.set(0,4,4);
+
+  const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light.position.set( 1, 10, 6);
   scene.add(light);
-  
-  const albedoMap = new THREE.TextureLoader().load(`${assetPath}TexturesCom_Orange_512_albedo.jpg`);
-  const normalMap = new THREE.TextureLoader().load(`${assetPath}TexturesCom_Orange_512_normal.jpg`);
-  
-  renderer = new THREE.WebGLRenderer();
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize( window.innerWidth, window.innerHeight );
+  render.domElement.id = "Canvas"
   document.body.appendChild( renderer.domElement );
-  
-  const controls = new THREE.OrbitControls( camera, renderer.domElement );
-  
-  //Add meshes here
-  const geometry = new THREE.SphereGeometry(1, 30, 20);
-  phongMaterial = new THREE.MeshPhongMaterial();
-  standardMaterial = new THREE.MeshStandardMaterial();
-  
-  const phongSphere = new THREE.Mesh( geometry, phongMaterial);
-  const standardSphere = new THREE.Mesh( geometry, standardMaterial);
-  
-  for(let xPos=-3; xPos<3; xPos+=3){
-    const sphereA = phongSphere.clone();
-    sphereA.position.set(xPos, 1.5, 0);
-    scene.add(sphereA);
-    
-    if (xPos==0){
-      faceNormalsHelper = new THREE.FaceNormalsHelper(sphereA, 0.25);
-      vertexNormalsHelper = new THREE.VertexNormalsHelper(sphereA, 0.25);
-      faceNormalsHelper.visible = false;
-      vertexNormalsHelper.visible = false;
-      scene.add(faceNormalsHelper);
-      scene.add(vertexNormalsHelper);
-    }
-    
-    const sphereB = standardSphere.clone();
-    sphereB.position.set(xPos, -1.5, 0);
-    scene.add(sphereB);
-  }
-  
-  params1 = {
-    color: 0xffffff,
-    envMap: 'none',
-    reflectivity: 1,
-    albedoMap: 'none',
-    normalMap: 'none',
-    normalScale: 1,
-    shininess: 30,
-    facetted: false,
-    normals: 'none'
-  }
-  params2 = {
-    color: 0xffffff,
-    emissive: 0,
-    envMap: 'none',
-    reflectivity: 1,
-    albedoMap: 'none',
-    normalMap: 'none',
-    normalScale: 1,
-    roughness: 0.5,
-    metalness: 0.5,
-    facetted: false
-  }
-  
-  const gui = new dat.gui.GUI();
-  
-  gui.add(params1, 'normals', ['none', 'face', 'vertex']).onChange(function(value){
-    faceNormalsHelper.visible = false;
-    vertexNormalsHelper.visible = false;
-    phongMaterial.wireframe = false;
-    switch(value){
-      case 'face':
-        faceNormalsHelper.visible = true;
-        phongMaterial.wireframe = true;
-        break;
-      case 'vertex':
-        vertexNormalsHelper.visible = true;
-        phongMaterial.wireframe = true;
-        break;
-    }
-  });
-  const f1 = gui.addFolder('Phong Material');
-  f1.addColor(params1, 'color').onChange( function() { phongMaterial.color.set( params1.color ); } );
-  f1.add(params1, 'envMap', ['none', 'cathedral']).onChange( function(){
-    switch (params1.envMap){
-      case 'cathedral':
-        phongMaterial.envMap = envMap;
-        break;
-      default:
-        phongMaterial.envMap = null;
-        break;
-    }
-    phongMaterial.needsUpdate = true;
-  });
-  f1.add(params1, 'reflectivity').min(0).max(1).step(0.01).onChange( function(){ phongMaterial.reflectivity = params1.reflectivity });
-  f1.open();
-  f1.add(params1, 'albedoMap', ['none', 'orange']).onChange( function(value){
-    switch (value){
-      case 'orange':
-        phongMaterial.map = albedoMap;
-        break;
-      default:
-        phongMaterial.map = null;
-        break;
-    }
-    phongMaterial.needsUpdate = true;
-  });
-  f1.add(params1, 'normalMap', ['none', 'dimples']).onChange( function(value){
-    switch (value){
-      case 'dimples':
-        phongMaterial.normalMap = normalMap;
-        break;
-      default:
-        phongMaterial.normalMap = null;
-        break;
-    }
-    phongMaterial.needsUpdate = true;
-  });
-  f1.add(params1, 'normalScale').min(0).max(1).step(0.01).onChange( function(value){ phongMaterial.normalScale.x = value;
-phongMaterial.normalScale.y = value;                                });
-  f1.add(params1, 'shininess').min(0).max(255).step(0.5).onChange( function(value){ phongMaterial.shininess = value });
-  f1.add(params1, 'facetted').onChange( function(value){ 
-    phongMaterial.flatShading = value;
-    phongMaterial.needsUpdate = true;
-  });
-  
-  const f2 = gui.addFolder('Standard Material');
-   f2.addColor(params2, 'color').onChange( function(value) { standardMaterial.color.set( value ); } );
-  f2.addColor(params2, 'emissive').onChange( function(value) { standardMaterial.emissive.set( value ); } );
-  f2.add(params2, 'envMap', ['none', 'cathedral']).onChange( function(value){
-    switch (value){
-      case 'cathedral':
-        standardMaterial.envMap = envMap;
-        break;
-      default:
-        standardMaterial.envMap = null;
-        break;
-    }
-    standardMaterial.needsUpdate = true;
-  });
-  f2.add(params2, 'albedoMap', ['none', 'orange']).onChange( function(value){
-    switch (value){
-      case 'orange':
-        standardMaterial.map = albedoMap;
-        break;
-      default:
-        standardMaterial.map = null;
-        break;
-    }
-    standardMaterial.needsUpdate = true;
-  });
-  f2.add(params2, 'normalMap', ['none', 'dimples']).onChange( function(value){
-    switch (value){
-      case 'dimples':
-        standardMaterial.normalMap = normalMap;
-        break;
-      default:
-        standardMaterial.normalMap = null;
-        break;
-    }
-    standardMaterial.needsUpdate = true;
-  });
-  f2.add(params2, 'normalScale').min(0).max(1).step(0.01).onChange( function(value){ standardMaterial.normalScale.x = value;
-standardMaterial.normalScale.y = value;                                });
-  f2.add(params2, 'roughness').min(0).max(1).step(0.01).onChange( function(value){ standardMaterial.roughness = value });
-  f2.add(params2, 'metalness').min(0).max(1).step(0.01).onChange( function(value){ standardMaterial.metalness = value });
-  f2.add(params1, 'facetted').onChange( function(value){ 
-    standardMaterial.flatShading = value;
-    standardMaterial.needsUpdate = true;
-  });
-  
-  window.addEventListener( 'resize', resize, false);
-  
+  animate();
+
+  window.addEventListener('resize', resize, false);
+
   update();
+
 }
+
+function animate() {
+
+skybox.rotation.x += 0.005;
+skybox.rotation.y += 0.005;
+renderer.render(scene, camera);
+requestAnimationFrame(animate);
+}
+
+init();
+
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.addEventListener('change', render);
+  controls.target.set(0,4,0);
+  controls.update();
+
+  //Add meshes here
+  const height = 0.4;
+  const geometry = new THREE.BoxGeometry(3, height, 0.9);
+  const material = new THREE.MeshLambertMaterial({ color: 0x1bbac5 });
+  const mesh = new THREE.Mesh( geometry, material);
+  //scene.add(mesh);
+
+ // for(let row=0; row<13; row++){
+   // let yPos = row * ( height + 0.02);
+   // let offset = -1;
+   // for(let count=0; count<3; count++){
+   //   const block = mesh.clone();
+   //   if (row % 2){
+    //    block.rotation.y = Math.PI/2;
+    //    block.position.set(offset, yPos, 0);
+   //   }else{
+     //   block.position.set(0, yPos, offset);
+   //   }
+   //   scene.add(block);
+  //    offset++;
+  //  }
+ // }
 
 function update(){
   requestAnimationFrame( update );
